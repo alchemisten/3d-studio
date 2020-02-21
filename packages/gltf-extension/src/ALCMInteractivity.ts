e94706fc72e3418cbaede7cbbfb754103b8d4ee3
Florian Decker <florian.decker@lhind.dlh.de>
2020-02-21 13:39:53 +0100

    Added missing array modifier

--- packages/gltf-extension/src/ALCMInteractivity.ts
+++ packages/gltf-extension/src/ALCMInteractivity.ts
@@ -2,7 +2,7 @@ import {MaterialOptionGroup} from "./MaterialProperties";
 
 export interface Interactivity {
     i18n: LanguageMap;
-    materialOptionGroups: MaterialOptionGroup
+    materialOptionGroups: MaterialOptionGroup[]
 }
 
 export type LanguageMap = { [lang: string]: { [key: string]: string } };
