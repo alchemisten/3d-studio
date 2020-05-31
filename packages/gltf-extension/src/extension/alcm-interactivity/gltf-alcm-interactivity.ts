c9bb93bc3ea12a4c36536bb0e798560669ef3db1
FlorianDe <florian.decker@uni-oldenburg.de>
2020-05-31 13:56:58 +0200

    Enhanced documentation, minor fixes, anchor extension improvements

--- packages/gltf-extension/src/extension/alcm-interactivity/gltf-alcm-interactivity.ts
+++ packages/gltf-extension/src/extension/alcm-interactivity/gltf-alcm-interactivity.ts
@@ -8,6 +8,7 @@ import { MaterialOptionGroup } from "./materials-extension";
  * @member anchorExtension - Contains an array of the individual anchors for all scenes and their objects and the possible combinations among them.
  * @member viewerFeatures - Contains all possible viewer functions that can be activated/deactivated for the project separately and have a default start value.
  * @member projectInformation - Describes additonal project information @see ProjectInformation.
+ * @member highlights - Contains all highlights for each scene. The highlights do not have to be bound to a specific node in the hierarchy, they can also stand alone.
  */
 export interface Interactivity {
     i18n: Internationalization
@@ -55,11 +56,18 @@ export interface ProjectInformation {
     tags: [string]
 }
 
+/**
+ * Preservation of all information for internationalization.
+ * 
+ * @member supportedLocales - An array with each supported or enabled locale.
+ * @member languageMap - The i18n dictionary containing the translations for specific locales and keys (@see LanguageMap.)
+ */
 export interface Internationalization {
-    supportedLocales: String
+    supportedLocales: String[]
     languageMap: LanguageMap
 }
 
+// TODO (FlorianDe): Discuss
 export enum Positioning {
     Top = 0.0,
     Left = 0.0,
@@ -68,12 +76,26 @@ export enum Positioning {
     Right = 1.0
 }
 
+/**
+ * Possible values to tell the viewer implementation how the project should ideally be displayed.
+ * 
+ * Portrait - The project should be displayed in portrait mode.
+ * Landscape - The project should be displayed in landscape mode.
+ * All - The project can either be displayed in portrait or landscape mode.
+ */
 export enum ViewOrientation {
     Portrait = "PORTRAIT",
     Landscape = "LANDSCAPE",
     All = "ALL"
 }
 
+/**
+ * Influences the behavior of how a certain property of an object should be transformed in correlation with the set transformation type.
+ * 
+ * Local - Should be used if the property is to be transformed into correlation to the hierarchy.
+ * Global - Should be used if the property shall have no hierarchical dependency.
+ * Fixed - Can be used to restrict any transformation for a property. E.g. to place a highlight always at a specific position on the screen.
+ */
 export enum LocationTransformType {
     Local = "LOCAL",
     Global = "GLOBAL",
@@ -82,26 +104,45 @@ export enum LocationTransformType {
 
 export type ColorString = string
 export type TemplateString = string
-export type Key_I18N = string
 
-export type LanguageMap = { [lang: string]: { [key: string]: string } };
 /**
- * N:M Mapping for anchors gltfIds
+ * Type description for a dictionary.
+ * 
+ * It is a nested map structure where the first key represents a locale value and the second key is a translation key to retrieve the translation.
+ */
+export type LanguageMap = { [lang: string]: { [key: string]: string } };
+
+/**
+ * Type description for a n:m number map.
+ * 
+ * N:M mapping for compatible anchors with their corresponding gltfIds
  */
 export type AnchorMap = { [anchorId: number]: number[] };
 
+/**
+ * Type description for an anchor object.
+ * 
+ * @member id - A unique identifier to exactly specify 
+ * @member name - The name for an anchor which could be used to specify or display it. The name can be a templateable string to allow dynamic content and/or internationalization.
+ * @member categories - An array of categories this anchor belongs to. This field can also be empty if an anchor has no suitable group/category
+ * @member matrix - Matrix to describe an anchor point, translation, direction, up vector.
+ * @member transformationType - The transformation type for the anchor position in relation to the scene/hierarchy (@see LocationTransformType).
+ */
 export interface Anchor {
-    name: Key_I18N
-
-    /**
-     * Matrix to describe an anchor point, translation, direction, up vector.
-     *
-     * @schemaInclude:Accessor.type.MAT4
-     * */
+    id: string
+    name?: TemplateString
+    categories?: string[]
+    /**@schemaInclude:Accessor.type.MAT4 */
     matrix: number[]
     transformationType: LocationTransformType
 }
 
+/**
+ * Includes all components regarding the anchor extension.
+ * 
+ * @member anchors - An array with anchors for all scenes.
+ * @member anchorCompatibilities - The compatability n:m map for suitable anchors (@see AnchorMap.)
+ */
 export interface AnchorExtension {
     anchors: Anchor[]
     anchorCompatibilities: AnchorMap
@@ -120,9 +161,13 @@ export interface ViewerFeatures {
     usePostProcessing: ViewerFeature<boolean>
     useTransparency: ViewerFeature<boolean>
     useFog: ViewerFeature<FogOptions>
-    useBackground: ViewerFeature<boolean> //TODO  Could specify a more convenient type to allow selection of a background
-    useEnvironment: ViewerFeature<boolean> //TODO  Could specify a more convenient type to allow selection of an environment
+    useBackground: ViewerFeature<boolean> // TODO (FlorianDe): Could specify a more convenient type to allow selection of a background
+    useEnvironment: ViewerFeature<boolean> // TODO (FlorianDe): Could specify a more convenient type to allow selection of an environment
 }
+
+/**
+ * This interface describes a generic feature for a viewer implementation which can be enabled/disabled for a project and which has as set default value.
+ */
 export interface ViewerFeature<T> {
     active: boolean,
     default: T
@@ -163,13 +208,13 @@ export interface Highlight {
         fov: number
         anchor: string
     }
-    isAnimationTrigger: boolean //TODO necessary?
+    isAnimationTrigger: boolean // TODO (FlorianDe): necessary?
     triggerAnimation: {
         fromBeginning?: boolean
-        animationId: number //should be under animations/
+        animationId: number //should be under animations/x
         delay?: number
-        endTime?: string //TODO define format
-        relativeDuration?: string //TODO define format
+        endTime?: string // TODO (FlorianDe): define format
+        relativeDuration?: string // TODO (FlorianDe): define format
         visibleThroughAnimation: boolean
     }
     isCameraPanTrigger: boolean //necessary?
