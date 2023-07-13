af4a9a8b7a588bb3778549dc38b0bed71c9c6cbd
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2023-07-13 12:53:34 +0200

    feat: Add animation interface

---
+++ libs/viewer-ui/src/components/animation-bar/animation-bar.tsx
@@ -0,0 +1,163 @@
+import { FC, useEffect, useState } from 'react';
+import { AnimationAction, AnimationClip } from 'three';
+import { timer } from 'rxjs';
+import { debounce } from 'rxjs/operators';
+import { getTrackBackground, Range } from 'react-range';
+
+import { Button } from '../button/button';
+import { useViewerContext } from '../../provider/viewer.provider';
+import styles from './animation-bar.module.scss';
+
+interface AnimationData {
+  action?: AnimationAction;
+  duration: number;
+  isRunning: boolean;
+  time: number;
+}
+
+export const AnimationBar: FC = () => {
+  const [animation, setAnimation] = useState<AnimationClip | null>(null);
+  const [animationData, setAnimationData] = useState<AnimationData | null>(null);
+  const [animations, setAnimations] = useState<AnimationClip[]>([]);
+  const viewer = useViewerContext();
+
+  useEffect(() => {
+    if (!viewer) {
+      return;
+    }
+    const subscription = viewer.assetService.hookObjectLoaded$.subscribe((object) => {
+      if (object.animations.length > 0) {
+        const mixer = viewer.animationService.addMixerForObject(object);
+        if (mixer) {
+          setAnimations(object.animations);
+          setAnimation(object.animations[0]);
+
+          const action = viewer?.animationService.playObjectAnimation({
+            animationName: object.animations[0].name,
+            objectName: object.name,
+          });
+          viewer?.animationService.setAnimationEnabled(false);
+          if (action) {
+            setAnimationData({
+              action,
+              duration: object.animations[0].duration,
+              isRunning: false,
+              time: action.time,
+            });
+          }
+        }
+      }
+    });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [viewer]);
+
+  useEffect(() => {
+    if (!viewer) {
+      return;
+    }
+
+    const subscription = viewer.animationService
+      .getActiveAnimationTime()
+      .pipe(debounce(() => timer(10)))
+      .subscribe((animationMap) => {
+        if (animation && animationMap[animation.name] && animationData) {
+          // console.log('Setting animation data');
+          setAnimationData({
+            ...animationData,
+            time: animationMap[animation.name].time,
+          });
+        }
+      });
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [viewer, animation, animationData]);
+
+  const setAnimationTime = (time: number) => {
+    viewer?.animationService.setAnimationTime(time);
+  };
+
+  const toggleAnimation = () => {
+    if (animationData?.action) {
+      const isRunning = animationData.action.isRunning();
+      viewer?.animationService.setAnimationEnabled(!isRunning);
+      setAnimationData({
+        ...animationData,
+        isRunning: !isRunning,
+      });
+    }
+  };
+
+  if (animations.length === 0) {
+    return null;
+  }
+
+  const max = animation?.duration || 1;
+
+  return (
+    <div className={styles.animationBar}>
+      <div className={styles.rangeSlider}>
+        <Range
+          max={animation?.duration || 1}
+          onChange={(values) => setAnimationTime(values[0])}
+          step={0.001}
+          values={[animationData?.time || 0]}
+          renderTrack={({ props, children }) => (
+            <div className={styles.barWrapper} {...props} style={props.style}>
+              <div
+                className={styles.bar}
+                style={{
+                  background: getTrackBackground({
+                    values: [animationData?.time || 0],
+                    colors: ['var(--ui-color-interaction)', '#fff'],
+                    min: 0,
+                    max: animation?.duration || 1,
+                  }),
+                }}
+              >
+                {children}
+              </div>
+            </div>
+          )}
+          renderThumb={({ props, value }) => (
+            <div
+              {...props}
+              className={styles.handle}
+              style={{
+                ...props.style,
+                left: `${(value / max) * 100}%`,
+              }}
+            />
+          )}
+        />
+      </div>
+
+      <div className={styles.controls}>
+        <Button onClick={toggleAnimation}>
+          {animationData?.action && animationData.isRunning ? (
+            <svg fill="#ffffff" height="24" width="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <rect y="5" x="6" width="4" height="14"></rect>
+              <rect y="5" x="14" width="4" height="14"></rect>
+            </svg>
+          ) : (
+            <svg fill="currentColor" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+              <path d="M8 5v14l11-7z"></path>
+              <path d="M0 0h24v24H0z" fill="none"></path>
+            </svg>
+          )}
+        </Button>
+
+        {animationData && (
+          <div className={styles.time}>
+            <span className={styles.currentTime}>{(Math.floor(animationData.time * 100) / 100).toFixed(2)}</span> /
+            <span className={styles.totalTime}>{(Math.floor(animationData.duration * 100) / 100).toFixed(2)}</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
--- libs/viewer-ui/src/lib/viewer-ui.tsx
+++ libs/viewer-ui/src/lib/viewer-ui.tsx
@@ -2,10 +2,13 @@ import { IViewer } from '@alchemisten/3d-studio-viewer-core';
 import React, { FC, useEffect, useState } from 'react';
 import { TranslationsProvider } from 'react-intl-provider';
 import { FormattedMessage } from 'react-intl';
+import { Subscription } from 'rxjs';
 
 import { Controls, TextBox } from '../components';
 import { translations } from '../i18n';
+import { ViewerProvider } from '../provider/viewer.provider';
 import { LoadingIndicator } from '../components/loading-indictator/loading-indicator';
+import { AnimationBar } from '../components/animation-bar/animation-bar';
 import styles from './viewer-ui.module.scss';
 
 export interface ViewerUIProps {
@@ -13,7 +16,7 @@ export interface ViewerUIProps {
 }
 
 export const ViewerUI: FC<ViewerUIProps> = ({ viewer }) => {
-  const [introClosed, setIntroClosed] = useState(false);
+  const [introClosed, setIntroClosed] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const [objectLoaded, setObjectLoaded] = useState(false);
 
@@ -24,25 +27,38 @@ export const ViewerUI: FC<ViewerUIProps> = ({ viewer }) => {
   };
 
   useEffect(() => {
-    viewer.assetService.getIsLoading().subscribe((loading) => {
-      setIsLoading(loading);
-    });
-    viewer.assetService.hookObjectLoaded$.subscribe((loaded) => {
-      setObjectLoaded(true);
-    });
+    const subscription = new Subscription();
+
+    subscription.add(
+      viewer.assetService.getIsLoading().subscribe((loading) => {
+        setIsLoading(loading);
+      })
+    );
+    subscription.add(
+      viewer.assetService.hookObjectLoaded$.subscribe(() => {
+        setObjectLoaded(true);
+      })
+    );
+
+    return () => {
+      subscription.unsubscribe();
+    };
   }, [viewer]);
 
   return (
     <TranslationsProvider initialLanguage="de" initialTranslations={translations}>
-      <div className={`${styles.viewerUi} ${introClosed ? styles.clicked : ''}`} onClick={handleUIClick}>
-        <Controls />
-        {!introClosed && (
-          <TextBox position="intro">
-            {isLoading && <LoadingIndicator />}
-            {objectLoaded && <FormattedMessage id="intro" />}
-          </TextBox>
-        )}
-      </div>
+      <ViewerProvider viewer={viewer}>
+        <div className={`${styles.viewerUi} ${introClosed ? styles.clicked : ''}`} onClick={handleUIClick}>
+          <Controls />
+          {!introClosed && (
+            <TextBox position="intro">
+              {isLoading && <LoadingIndicator />}
+              {objectLoaded && <FormattedMessage id="intro" />}
+            </TextBox>
+          )}
+          <AnimationBar />
+        </div>
+      </ViewerProvider>
     </TranslationsProvider>
   );
 };
