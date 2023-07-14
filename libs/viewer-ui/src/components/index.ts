fcb869ed4dc3375413851bbe0bf02c99d82b3389
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2023-07-14 15:03:45 +0200

    feat: Add feature control component

--- libs/viewer-ui/src/components/index.ts
+++ libs/viewer-ui/src/components/index.ts
@@ -3,6 +3,7 @@ export * from './button/button';
 export * from './button-toggle/button-toggle';
 export * from './button-radio-group/button-radio-group';
 export * from './controls/controls';
+export * from './feature-control/feature-control';
 export * from './loading-indictator/loading-indicator';
 export * from './select-box/select-box';
 export * from './text-box/text-box';
