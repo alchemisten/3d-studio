68a96d69dd4c0e4078097bdd212ea5db8cbeae2c
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2023-07-17 12:56:41 +0200

    fix: Fix inverted select box button direction

--- libs/viewer-ui/src/components/select-box/select-box.tsx
+++ libs/viewer-ui/src/components/select-box/select-box.tsx
@@ -42,7 +42,7 @@ export const SelectBox: FC<SelectBoxProps> = ({ currentEntry, entries, onEntrySe
         </div>
       )}
 
-      <button type="button" className={styles.prev} onClick={selectNextEntry}>
+      <button type="button" className={styles.prev} onClick={selectPreviousEntry}>
         <svg fill="currentColor" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
           <path d="M15.41 16.09l-4.58-4.59 4.58-4.59L14 5.5l-6 6 6 6z"></path>
           <path d="M0-.5h24v24H0z" fill="none"></path>
@@ -53,7 +53,7 @@ export const SelectBox: FC<SelectBoxProps> = ({ currentEntry, entries, onEntrySe
         {currentEntry?.label || ''}
       </button>
 
-      <button type="button" className={styles.next} onClick={selectPreviousEntry}>
+      <button type="button" className={styles.next} onClick={selectNextEntry}>
         <svg fill="currentColor" height="20" viewBox="0 0 24 24" width="20" xmlns="http://www.w3.org/2000/svg">
           <path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"></path>
           <path d="M0-.25h24v24H0z" fill="none"></path>
