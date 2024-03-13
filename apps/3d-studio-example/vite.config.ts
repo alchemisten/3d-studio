c42cb8ba1d493b78b517586a77c33edb3fc3e5c7
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2024-03-13 16:14:34 +0100

    build: Update NX and Three JS to most recent versions

--- apps/3d-studio-example/vite.config.ts
+++ apps/3d-studio-example/vite.config.ts
@@ -1,8 +1,17 @@
 /// <reference types="vitest" />
+import replaceFiles from '@nx/vite/plugins/rollup-replace-files.plugin';
 import { defineConfig } from 'vite';
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 
 export default defineConfig({
+  root: __dirname,
+  build: {
+    outDir: '../../dist/apps/3d-studio-example',
+    reportCompressedSize: true,
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
   cacheDir: '../../.vite/3d-studio-example',
 
   server: {
@@ -15,7 +24,15 @@ export default defineConfig({
     host: 'localhost',
   },
 
-  plugins: [nxViteTsPaths()],
+  plugins: [
+    nreplaceFiles([
+      {
+        replace: 'apps/3d-studio-example/src/environments/environment.ts',
+        with: 'apps/3d-studio-example/src/environments/environment.prod.ts',
+      },
+    ]),
+    xViteTsPaths(),
+  ],
 
   // Uncomment this if you are using workers.
   // worker: {
@@ -27,6 +44,11 @@ export default defineConfig({
   // },
 
   test: {
+    reporters: ['default'],
+    coverage: {
+      reportsDirectory: '../../coverage/apps/3d-studio-example',
+      provider: 'v8',
+    },
     globals: true,
     cache: {
       dir: '../../.vitest',
--- libs/viewer-core/vite.config.ts
+++ libs/viewer-core/vite.config.ts
@@ -5,6 +5,7 @@ import dts from 'vite-plugin-dts';
 import { joinPathFragments } from '@nx/devkit';
 
 export default defineConfig({
+  root: __dirname,
   cacheDir: '../../node_modules/.vite/viewer-core',
 
   plugins: [
@@ -28,6 +29,9 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
+    outDir: '../../dist/libs/viewer-core',
+    reportCompressedSize: true,
+    commonjsOptions: { transformMixedEsModules: true },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
       entry: 'src/index.ts',
