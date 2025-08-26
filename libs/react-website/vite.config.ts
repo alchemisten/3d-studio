d9074862c21283638f1484b966369d9de3e68aed
Gordon Schroeder <gordon.schroeder@alchemisten.ag>
2025-08-26 14:24:38 +0200

    build: Update eslint stuff and project files

--- libs/react-website/vite.config.ts
+++ libs/react-website/vite.config.ts
@@ -5,6 +5,17 @@ import viteTsConfigPaths from 'vite-tsconfig-paths';
 import dts from 'vite-plugin-dts';
 import { joinPathFragments } from '@nx/devkit';
 
+// These options were migrated by @nx/vite:convert-to-inferred from the project.json file.
+const configValues = { default: {}, development: {}, production: {} };
+
+// Determine the correct configValue to use based on the configuration
+const nxConfiguration = process.env.NX_TASK_TARGET_CONFIGURATION ?? 'default';
+
+const options = {
+  ...configValues.default,
+  ...(configValues[nxConfiguration] ?? {}),
+};
+
 export default defineConfig({
   cacheDir: '../../node_modules/.vite/react-website',
 
--- libs/viewer-ui/vite.config.ts
+++ libs/viewer-ui/vite.config.ts
@@ -5,6 +5,17 @@ import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import dts from 'vite-plugin-dts';
 import { joinPathFragments } from '@nx/devkit';
 
+// These options were migrated by @nx/vite:convert-to-inferred from the project.json file.
+const configValues = { default: {}, development: {}, production: {} };
+
+// Determine the correct configValue to use based on the configuration
+const nxConfiguration = process.env.NX_TASK_TARGET_CONFIGURATION ?? 'default';
+
+const options = {
+  ...configValues.default,
+  ...(configValues[nxConfiguration] ?? {}),
+};
+
 export default defineConfig({
   root: __dirname,
   cacheDir: '../../node_modules/.vite/viewer-ui',
